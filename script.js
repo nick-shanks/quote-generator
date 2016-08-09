501d0aeb2c2fdc8f85ebf078a7d113c804a040d2
Andrew Hird <andrew.james.hird@gmail.com>
2016-08-09 20:33:17 +1200

    quote showing up

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// var quote = document.getElementsByClassName('quote')
+
+// $.ajax({
+//     url: 'MASHAPE-URL', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+//     type: 'POST', // The HTTP Method
+//     data: {}, // Additional parameters here
+//     datatype: 'json',
+//     success: function(data) { alert(JSON.stringify(data)); },
+//     error: function(err) { alert(err); },
+//     beforeSend: function(xhr) {
+//     xhr.setRequestHeader("X-Mashape-Authorization", "YOUR-MASHAPE-KEY"); // Enter here your Mashape key
+//     }
+// });
+
+
+
+
 
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies/", false);
-xhr.setRequestHeader("X-Mashape-Authorization", "M2TjGCFFdymshrsUeT83gsnpDChWp18pzKpjsnVY7luUvFA0mU")
+xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/", false);
+xhr.setRequestHeader("X-Mashape-Authorization", "bPB3LWtkZDmsh10m2wLeFCOzkCR5p1sc4XzjsncgJVwxESLMVs")
 xhr.onload = function() {
     if (xhr.status === 200) {
         document.getElementById("quote").innerHTML = xhr.responseText
